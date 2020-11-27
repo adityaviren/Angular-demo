294139be4b709b97d119959ec082c907915f1525
Aditya Viren <adityaviren@gmail.com>
2020-11-27 23:13:55 +0530

    Initialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RadioComponent } from './component/radio/radio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'radio',component:RadioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
